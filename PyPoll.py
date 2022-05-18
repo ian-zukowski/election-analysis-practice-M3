34c54f1b201359f1fd79e85fb293a2e47c2826c6
ian-zukowski <104320798+ian-zukowski@users.noreply.github.com>
2022-05-18 17:46:48 -0400

    Wrote results to txt_file.

--- PyPoll.py
+++ PyPoll.py
@@ -46,41 +46,60 @@ with open(file_to_load) as election_data:
         #Add one each time that name shows up
         candidate_votes[candidate_name]+=1
 
-# # print statement saying each candidate and how many votes they recieved
-# for candidate_name in candidate_options:
-#     print(f"Candidate {candidate_name} recieved {candidate_votes[candidate_name]:,} votes.")
+#Save the results to the txt file
+with open(file_to_save,'w') as txt_file:
+    election_results=(
+        f"\n"
+        f"Election Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"-------------------------\n"
+    )
+    print(election_results,end='')
+    txt_file.write(election_results)
+
+    # # print statement saying each candidate and how many votes they recieved
+    # for candidate_name in candidate_options:
+    #     print(f"Candidate {candidate_name} recieved {candidate_votes[candidate_name]:,} votes.")
 
 
-# get percentage of votes for each candidate in candidate_options
-for candidate_name in candidate_options:
-    #set variable to gather vote amounts from the dictionary
-    votes=candidate_votes[candidate_name]
-    #takes votes/total*100
-    vote_percentage=(votes/total_votes)*100
-    # #print each candidate and how much % of the votes they recieved
-    # print(f"{candidate_name} recieved {vote_percentage:.2f}% of the votes.")
+    # get percentage of votes for each candidate in candidate_options
+    for candidate_name in candidate_options:
+        #set variable to gather vote amounts from the dictionary
+        votes=candidate_votes[candidate_name]
+        #takes votes/total*100
+        vote_percentage=(votes/total_votes)*100
+        # #print each candidate and how much % of the votes they recieved
+        # print(f"{candidate_name} recieved {vote_percentage:.2f}% of the votes.")
 
-    #print statement-- [Candidate]: [Percentage Earned] and [Vote Count]
-    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes})")
+        #print statement-- [Candidate]: [Percentage Earned] and [Vote Count]
+        #print(f"{candidate_name}: {vote_percentage:.1f}% ({votes})")
+        candidate_results=(
+            f"{candidate_name}: {vote_percentage:.1f}% ({votes})\n"
+        )
 
-    #finding winning values for candidate, count, and percentage
-    #doesn't it only need one of these two conditions to be true??
-    if (vote_percentage>winning_percentage) and (votes>winning_count):
-        winning_candidate=candidate_name
-        winning_count=votes
-        winning_percentage=vote_percentage
+        print(candidate_results)
+        txt_file.write(candidate_results)
 
-print("")
+        #finding winning values for candidate, count, and percentage
+        #doesn't it only need one of these two conditions to be true??
+        if (vote_percentage>winning_percentage) and (votes>winning_count):
+            winning_candidate=candidate_name
+            winning_count=votes
+            winning_percentage=vote_percentage
 
-# my winning message
-print(f"{winning_candidate} won the election with {winning_count:,} votes.\n{winning_candidate} recieved {winning_percentage:.2f}% of the votes.")
 
-# their winning message
-winning_summary=(
-    f"-------------------------\n"
-    f"Winner: {winning_candidate}\n"
-    f"Winning Vote Count: {winning_count:,}\n"
-    f"Winning Percentage: {winning_percentage:.1f}%\n"
-    f"-------------------------\n"
-)
-print(winning_summary)
+    # # my winning message
+    # print(f"{winning_candidate} won the election with {winning_count:,} votes.\n{winning_candidate} recieved {winning_percentage:.2f}% of the votes.")
+
+    # their winning message
+    winning_summary=(
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"-------------------------\n"
+    )
+    #print(winning_summary)
+
+    txt_file.write(winning_summary)
